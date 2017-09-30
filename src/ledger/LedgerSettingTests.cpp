f1ac76dc2f81363b039ae4b72e3a8cdbfc32dc88
astonshnam <drumhyun@gmail.com>
2017-09-30 14:52:31 +0900

    add test of baseFee when ledger manager makes a transaction

--- src/ledger/LedgerSettingTests.cpp
+++ src/ledger/LedgerSettingTests.cpp
@@ -18,7 +18,7 @@ typedef std::unique_ptr<Application> appPtr;
 
 TEST_CASE("ledgerSetting","[ledgerSetting]")
 {
-//    Config const& cfg = getTestConfig();
+    // Config const& cfg = getTestConfig();
     std::string cfgFile("docs/stellar-core_example2.cfg");
     Config cfg;
     cfg.load(cfgFile);
@@ -26,19 +26,28 @@ TEST_CASE("ledgerSetting","[ledgerSetting]")
     VirtualClock clock;
     ApplicationEditableVersion app(clock, cfg);
     app.start();
-		  
+
     auto root = TestAccount::createRoot(app);
-   
-    AccountFrame::pointer rootAccount;
-    rootAccount = loadAccount(root, app);
-	   
-    int64_t balance = rootAccount->getBalance();
+
+    int64_t balance = app.getLedgerManager().getCurrentLedgerHeader().totalCoins;
     int64_t baseFee = app.getLedgerManager().getTxFee();
-    int64_t baseReserve = app.getLedgerManager().getCurrentLedgerHeader().baseReserve;
+    int64_t baseReserve = 
+	app.getLedgerManager().getCurrentLedgerHeader().baseReserve;
     uint32_t maxTxSetSize = app.getLedgerManager().getMaxTxSetSize();
 
     REQUIRE(balance == cfg.BALANCE);
     REQUIRE(baseFee == cfg.BASE_FEE);
     REQUIRE(baseReserve == cfg.BASE_RESERVE);
     REQUIRE(maxTxSetSize == cfg.MAX_TX_SET_SIZE);
+
+    const int64_t paymentAmount = 
+        app.getLedgerManager().getMinBalance(0) + 10 * baseFee;
+    auto a1 = root.create("A", paymentAmount);
+
+    AccountFrame::pointer rootAccount, a1Account;
+    rootAccount = loadAccount(root, app);
+    a1Account = loadAccount(a1, app);
+
+    REQUIRE(a1Account->getBalance() == paymentAmount);
+    REQUIRE(rootAccount->getBalance() == (cfg.BALANCE - paymentAmount - baseFee));
 }
